f2885bfba9c3fbfe7290d25c50ff5050b33e770e
Dylan Peterson <peterson24246@yahoo.com>
2021-12-12 15:20:21 -0600

    Updated CORS to allow for localhost:3000

--- cardano_explorer_backend/settings.py
+++ cardano_explorer_backend/settings.py
@@ -135,5 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "https://localhost:8000",
+    "https://localhost:3000",
     "https://peterson24246.github.io"
 ]
