0e0aa1f9c71923cde775faad44244a34d99936c0
Dylan Peterson <peterson24246@yahoo.com>
2021-12-12 14:45:05 -0600

    Added new endpoint for latest block produced

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('blocks/latest', views.block_latest, name='block_latest')
 ]
--- api/views.py
+++ api/views.py
@@ -7,16 +7,24 @@ import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 api_key = os.environ.get('API_KEY')
+api = BlockFrostApi(
+        project_id=api_key
+)
 # Create your views here.
 
+# View returning json of network status
 def index(request):
-    api = BlockFrostApi(
-        project_id=api_key
-    )
-
     try:
         health = api.health(return_type='json')
     except ApiError as e:
         health = { 'error' : e }
 
     return JsonResponse(health)
+
+def block_latest(request):
+    try:
+        latest_block = api.block_latest(return_type="json")
+    except ApiError as e:
+        latest_block = {'error' : e }
+    
+    return JsonResponse(latest_block)
